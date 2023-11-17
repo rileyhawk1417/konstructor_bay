e95b59a7f66af983b69576845c1cbd8fb88d8374
mosesgitonga <infosec947@gmail.com>
2023-11-17 12:04:36 +0300

    working basemodel

---
+++ test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+test if the base model is returning output as exepected
+"""
+from models.base_model import BaseModel
+
+obj1 = BaseModel()
+obj2 = BaseModel()
+
+print(obj1.id)
+print(obj2.id)
+
+obj1.name = "Root"
+print(obj1.name)
+
+obj1.number = 34
+print(obj1.number)
+
+print(obj1)
+
+dict_test = obj1.to_dict()
+print(dict_test)
+
