53570b2192ae2ff2000def67bf8de66ebcb51b1a
Joseph Kimuchu <113996255+KimuchuJr@users.noreply.github.com>
2023-11-18 10:30:16 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,21 +1,18 @@
-#!/usr/bin/env python3
-"""
-user model
-"""
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel  # Assuming BaseModel is correctly defined
 
-class User(Base, BaseModel):
-    """
-    creating user on table name user
-    """
-    __tablename__ = "user"
+class User(BaseModel):
+    __tablename__ = "users"  # Adjusted table name to plural form
+
+    user_id = Column(Integer, primary_key=True)
     firstName = Column(String(18), nullable=False)
     sec_name = Column(String(18), nullable=False)
     username = Column(String(25), nullable=False)
     email = Column(String(256), nullable=True)
     phone_num = Column(String(20), nullable=False)
     password = Column(String(256), nullable=False)
+
+    # Adjusted ForeignKey reference corresponding to the Location table
+    location_id = Column(Integer, ForeignKey('locations.id'), nullable=False)
     location = relationship("Location", backref="user")
-    location_id = Column(String(256), ForeignKey("location.id"), nullable=False)
