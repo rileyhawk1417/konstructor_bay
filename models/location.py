38fc0d3bf66385fd1d6a390673997676ee58093d
Joseph Kimuchu <113996255+KimuchuJr@users.noreply.github.com>
2023-11-18 10:30:40 +0300

    Update location.py

--- models/location.py
+++ models/location.py
@@ -1,19 +1,14 @@
-#!/usr/bin/env python3
-"""
-location model
-"""
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel  # Assuming BaseModel is correctly defined
 
+class Location(BaseModel):
+    __tablename__ = "locations"  # Adjusted table name to plural form
 
-class Location(Base, BaseModel):
-    """
-    location for user, product
-    """
-    __tablename__ = "location"
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    user = relationship("User", backref="locations", cascade="all, delete-orphan")
+    id = Column(Integer, primary_key=True)  # Assuming 'id' is the primary key for Location
+    user_id = Column(String(50), ForeignKey('users.user_id'), nullable=False)  # Foreign key to User table
+    user = relationship("User", backref="locations")  # Relationship with User
+
     country = Column(String(24), nullable=False)
     county = Column(String(50), nullable=False)
     zone = Column(String(60), nullable=True)
