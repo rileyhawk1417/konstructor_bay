8395098e6ea0cf02551232caa252bec93ac4a5c1
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-11-20 18:16:45 +0200

    fix: add missing link for product page

--- frontend/src/components/product_grid.js
+++ frontend/src/components/product_grid.js
@@ -17,31 +17,33 @@ import Link from "next/link";
  *  Makes it easier instead of writing name, which is hard to use
  */
 
-function ProductCard(props) {
+export function ProductCard(props) {
   return (
-    <div className="card w-96  bg-base-100 shadow-xl" key={props.keyID}>
-      <figure>
-        <Image
-          src="/assets/images/traffic_cone_4.jpg"
-          height={500}
-          width={500}
-          alt="Traffic cone image"
-          loading="lazy"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Name: {props.itemName}</h2>
-        <p>{props.itemDesc}</p>
-        <p>Quantity: {props.quantity}</p>
-        <p>Supplier {props.supplierName}</p>
-        <div className="card-actions items-center justify-evenly">
-          <p className="card-title text-2xl">{props.itemPrice}</p>
-          <Link href="#">
-            <button className="btn btn-primary">Buy Now</button>
-          </Link>
+    <Link href={`/product/${props.itemID}`}>
+      <div className="card w-96  bg-base-100 shadow-xl" key={props.keyID}>
+        <figure>
+          <Image
+            src="/assets/images/traffic_cone_4.jpg"
+            height={500}
+            width={500}
+            alt="Traffic cone image"
+            loading="lazy"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Name: {props.itemName}</h2>
+          <p>{props.itemDesc}</p>
+          <p>Quantity: {props.quantity}</p>
+          <p>Supplier {props.supplierName}</p>
+          <div className="card-actions items-center justify-evenly">
+            <p className="card-title text-2xl">{props.itemPrice}</p>
+            <Link href="#">
+              <button className="btn btn-primary">Buy Now</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
