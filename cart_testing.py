00834f66d14fac25dd7bb82bf1ea83f1706bd2d2
KimuchuJr <kimuchukim@gmail.com>
2023-11-23 17:50:43 +0300

    Testing

--- cart_testing.py
+++ cart_testing.py
@@ -1,12 +1,82 @@
-#!/usr/bin/env python3
-"""
-cart  testing
-"""
+import unittest
+from cart_testing. import Cart
 
-from models.engine.cart_manager import Cart_manager
-from models import storage
+class TestCart(unittest.TestCase):
+    def setUp(self):
+        # Set up any necessary resources or configurations for tests
+        pass
 
-cm = Cart_manager()
+    def tearDown(self):
+        # Clean up after the tests
+        pass
 
-#create_cart(<user_id>, <product_id>)
-cm.create_Cart('1c24003b-64d6-485b-9a59-bafc81db081b', '2105c257-950c-49e8-9518-05537cb74755')
+    def test_initialization(self):
+        # Test initialization of Cart class
+        cart = Cart()
+        self.assertIsNone(cart.user_id)
+        self.assertIsNone(cart.product_id)
+
+        # Test initialization with specific values
+        cart_with_values = Cart(user_id='user_123', product_id='prod_456')
+        self.assertEqual(cart_with_values.user_id, 'user_123')
+        self.assertEqual(cart_with_values.product_id, 'prod_456')
+
+    def test_relationships(self):
+        # Test relationships of Cart class
+        cart = Cart()
+        self.assertIsInstance(cart.user, relationship)
+        self.assertIsInstance(cart.product, relationship)
+
+    def test_relationship_backref(self):
+        # Test backref in relationships
+        cart = Cart()
+        self.assertIsNone(cart.user)
+        self.assertIsNone(cart.product)
+
+        # Assuming 'User' and 'Product' classes have a similar relationship
+        # Test setting user and product
+        user_obj = User()
+        product_obj = Product()
+        cart.user = user_obj
+        cart.product = product_obj
+
+        self.assertEqual(cart.user, user_obj)
+        self.assertEqual(cart.product, product_obj)
+
+    # Add more test cases here to cover specific functionalities/methods of the Cart class
+    def test_add_to_cart(self):
+        # Test adding items to the cart
+        cart = Cart()
+        initial_cart_size = len(cart.products)
+
+        # Assuming 'Product' class exists and has an ID
+        product = Product(id='prod_789', name='Test Product', price=10.99)
+        cart.products.append(product)
+        self.assertEqual(len(cart.products), initial_cart_size + 1)
+        self.assertIn(product, cart.products)
+
+    def test_remove_from_cart(self):
+        # Test removing items from the cart
+        cart = Cart()
+        product = Product(id='prod_789', name='Test Product', price=10.99)
+        cart.products.append(product)
+        initial_cart_size = len(cart.products)
+
+        # Remove the product from the cart
+        cart.products.remove(product)
+        self.assertEqual(len(cart.products), initial_cart_size - 1)
+        self.assertNotIn(product, cart.products)
+
+    def test_cart_total(self):
+        # Test calculating the total price of items in the cart
+        cart = Cart()
+        product1 = Product(id='prod_789', name='Test Product 1', price=10.99)
+        product2 = Product(id='prod_790', name='Test Product 2', price=20.49)
+        cart.products.extend([product1, product2])
+
+        expected_total = product1.price + product2.price
+        self.assertAlmostEqual(cart.calculate_total(), expected_total, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
+
