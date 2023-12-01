28760aa5c88699d5ea36b18c65fe839aa62fd53a
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-12-01 14:51:45 +0200

    fix: commit changes to run locally

--- api/users.py
+++ api/users.py
@@ -6,29 +6,52 @@ from models import storage
 from models.user import User
 from flask import Blueprint, jsonify, request
 from models.engine.user_manager import User_manager
-users_bp = Blueprint('user', __name__, url_prefix='/api')
 
-@users_bp.route("/add_user", methods=['POST'], strict_slashes=False)
+users_bp = Blueprint("user", __name__, url_prefix="/api")
+
+
+@users_bp.route("/add_user", methods=["POST"], strict_slashes=False)
 def add_user():
     """
     adding a user to the site
     """
     data = request.get_json()
 
-    first_name = data.get('first_name')
-    second_name = data.get('second_name')
-    user_name = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-    #create_user(<firstName>, <secName>, user_name, email, password)
-    new_user = User_manager.create_user(first_name, second_name, user_name, email, password)
+    first_name = data.get("first_name")
+    second_name = data.get("second_name")
+    user_name = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+    # create_user(<firstName>, <secName>, user_name, email, password)
+    new_user = User_manager.create_user(
+        first_name, second_name, user_name, email, password
+    )
 
     if new_user:
         return jsonify("new user  added successfully"), 201
     else:
         return jsonify("Unable to add user"), 500
 
-@users_bp.route("/delete_user/<user_id>", methods=['DELETE'], strict_slashes=False)
+
+@users_bp.route("/signin", methods=["GET"], strict_slashes=False)
+def get_user():
+    """
+    adding a user to the site
+    """
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+    user = User_manager()
+    check_user = user.check_user(email, password)
+
+    if check_user:
+        return jsonify("Found user"), 201
+    else:
+        return jsonify("User not found"), 500
+
+
+@users_bp.route("/delete_user/<user_id>", methods=["DELETE"], strict_slashes=False)
 def delete_user(user_id):
     """
     deleting user from db
@@ -39,17 +62,19 @@ def delete_user(user_id):
         return jsonify("user not found"), 404
 
     um.delete_a_user(user_id)
-   
+
     return jsonify("user have been deleted"), 200
 
-@users_bp.route("/update_passwd/<user_id>", methods=['PUT'], strict_slashes=False)
+
+@users_bp.route("/update_passwd/<user_id>", methods=["PUT"], strict_slashes=False)
 def update_passwd(user_id):
     """
     updating passwd of the user
     """
     return jsonify(update_passwd(user_id))
 
-@users_bp.route('/users', methods=['GET'], strict_slashes=False)
+
+@users_bp.route("/users", methods=["GET"], strict_slashes=False)
 def list_all_users():
     """
     listing all users
@@ -57,6 +82,6 @@ def list_all_users():
     um = User_manager()
     return jsonify(um.read_users())
 
-    
-#if __name__ == "__main__":
- #   users_bp.run(debug=True)
+
+# if __name__ == "__main__":
+#   users_bp.run(debug=True)
