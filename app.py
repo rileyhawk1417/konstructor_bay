5ee0590e0fa85fa4da87623bcb3dcf113c58b148
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-12-05 19:18:54 +0200

    fix: enable CORS for the frontend
    - The frontend was not receiving data for login CORS was needed to be on.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask_cors import CORS
 from api.products import products_bp
 from api.users import users_bp
 from api.orders import orders_bp
 from api.search import search_bp
 from api.auth import auth_bp
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(search_bp)
