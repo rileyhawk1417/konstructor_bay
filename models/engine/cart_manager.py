6aa0002242d68e0c6c89df3521ca87e6ea7c64fb
mosesgitonga <infosec947@gmail.com>
2024-02-20 23:28:35 +0300

    added some documetation

--- models/engine/cart_manager.py
+++ models/engine/cart_manager.py
@@ -8,10 +8,21 @@ from models import storage
 from models.product import Product
 
 class Cart():
+    """
+    The cart is a dictionary
+    """
     def __init__(self):
         self.products_cart = {}
 
     def add_product(self, product_id: str, quantity: int) -> None:
+        """
+        adds product to cart dictionary
+        Args:
+            product_id -> id of product which will be added to cart
+            quantity -> quantity of the product that will be added to cart
+        Return:
+            returns None
+        """
         existing_product = storage.new_get(Product, product_id)
         name = existing_product.product_name
         if existing_product is None:
@@ -27,3 +38,4 @@ if __name__ == '__main__':
     c = Cart()
     c.add_product('2bfb4f40-8ef8-42b2-9765-37786567cecb', 5)
     c.add_product('2bfb4f40-8ef8-42b2-9765-37786567cecb', 3)
+    c.add_product('7a3b5521-2945-48f1-b9c4-1a0ec625c936', 8)
